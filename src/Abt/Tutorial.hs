a2094e7fd165b06ddf7c3f6bb685ca66c63522cc
Jonathan Sterling <jon@jonmsterling.com>
2015-01-01 12:08:21 -0800

    Flesh out tutorial with a small bidirectional type checker

--- src/Abt/Tutorial.hs
+++ src/Abt/Tutorial.hs
@@ -7,6 +7,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE UnicodeSyntax #-}
+{-# LANGUAGE ViewPatterns #-}
 
 module Abt.Tutorial where
 
@@ -17,7 +18,9 @@ import Abt.Concrete.LocallyNameless
 import Control.Applicative
 import Control.Monad.Trans.State.Strict
 import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Except
 import Data.Vinyl
+import Prelude hiding (pi)
 
 -- | We'll start off with a monad in which to manipulate ABTs; we'll need some
 -- state for fresh variable generation.
@@ -50,16 +53,25 @@ instance MonadVar Var M where
 --
 data Lang ns where
   LAM ∷ Lang '[S Z]
-  APP ∷ Lang '[Z,Z]
+  APP ∷ Lang '[Z, Z]
+  PI ∷ Lang '[Z, S Z]
+  UNIT ∷ Lang '[]
+  AX ∷ Lang '[]
 
 instance Show1 Lang where
   show1 = \case
     LAM → "lam"
     APP → "ap"
+    PI → "pi"
+    UNIT → "unit"
+    AX → "<>"
 
 instance HEq1 Lang where
   heq1 LAM LAM = Just Refl
   heq1 APP APP = Just Refl
+  heq1 PI PI = Just Refl
+  heq1 UNIT UNIT = Just Refl
+  heq1 AX AX = Just Refl
   heq1 _ _ = Nothing
 
 lam ∷ Tm Lang (S Z) → Tm0 Lang
@@ -67,6 +79,15 @@ lam e = LAM $$ e :& RNil
 
 app ∷ Tm0 Lang → Tm0 Lang → Tm0 Lang
 app m n = APP $$ m :& n :& RNil
+
+ax ∷ Tm0 Lang
+ax = AX $$ RNil
+
+unit ∷ Tm0 Lang
+unit = UNIT $$ RNil
+
+pi ∷ Tm0 Lang → Tm Lang (S Z) → Tm0 Lang
+pi α xβ = PI $$ α :& xβ :& RNil
 
 -- | A monad transformer for small step operational semantics.
 --
@@ -97,6 +118,7 @@ step tm =
       out m >>= \case
         LAM :$ xe :& RNil → xe // n
         _ → app <$> step m <*> pure n <|> app <$> pure m <*> step n
+    PI :$ α :& xβ :& RNil → pi <$> step α <*> pure xβ
     _ → stepsExhausted
 
 -- | The reflexive-transitive closure of a small-step operational semantics.
@@ -113,6 +135,57 @@ star f a =
 --
 eval ∷ Tm0 Lang → Tm0 Lang
 eval = runM . star step
+
+newtype JudgeT m α
+  = JudgeT
+  { runJudgeT ∷ ExceptT String m α
+  } deriving (Monad, Functor, Applicative, Alternative)
+
+instance MonadVar Var m ⇒ MonadVar Var (JudgeT m) where
+  fresh = JudgeT . ExceptT $ Right <$> fresh
+  named str = JudgeT . ExceptT $ Right <$> named str
+
+type Ctx = [(Var, Tm0 Lang)]
+
+raise ∷ Monad m ⇒ String → JudgeT m α
+raise = JudgeT . ExceptT . return . Left
+
+checkTy
+  ∷ Ctx
+  → Tm0 Lang
+  → Tm0 Lang
+  → JudgeT M ()
+checkTy g tm ty = do
+  let ntm = eval tm
+      nty = eval ty
+  (,) <$> out ntm <*> out nty >>= \case
+    (LAM :$ xe :& RNil, PI :$ α :& yβ :& RNil) → do
+      z ← fresh
+      ez ← xe // var z
+      βz ← yβ // var z
+      checkTy ((z,α):g) ez βz
+    (AX :$ RNil, UNIT :$ RNil) → return ()
+    _ → do
+      ty' ← inferTy g tm
+      if ty' === nty
+        then return ()
+        else raise "Type error"
+
+inferTy
+  ∷ Ctx
+  → Tm0 Lang
+  → JudgeT M (Tm0 Lang)
+inferTy g tm = do
+  out (eval tm) >>= \case
+    V v | Just (eval → ty) ← lookup v g → return ty
+        | otherwise → raise "Ill-scoped variable"
+    APP :$ m :& n :& RNil → do
+      inferTy g m >>= out >>= \case
+        PI :$ α :& xβ :& RNil → do
+          checkTy g n α
+          eval <$> xβ // n
+        _ → raise "Expected pi type for lambda abstraction"
+    _ → raise "Only infer neutral terms"
 
 -- | @λx.x@
 --
@@ -135,7 +208,12 @@ appTm = do
 -- @
 --
 main ∷ IO ()
-main =
+main = do
+  -- Try out the type checker
+  either fail print . runM . runExceptT . runJudgeT $ do
+    x ← fresh
+    checkTy [] (lam (x \\ var x)) (pi unit (x \\ unit))
+
   print . runM $ do
     mm ← appTm
     mmStr ← toString mm
