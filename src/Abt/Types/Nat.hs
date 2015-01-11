68680f922c8ee4d6e66e594f02d0efeef069eaa4
Jon Sterling <jon@jonmsterling.com>
2015-01-11 12:27:31 -0800

    Added Typeable instances for Nat

--- src/Abt/Types/Nat.hs
+++ src/Abt/Types/Nat.hs
@@ -1,6 +1,16 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
 module Abt.Types.Nat where
+
+import Data.Typeable
 
 data Nat
   = Z
   | S !Nat
+  deriving Typeable
+
+deriving instance Typeable Z
+deriving instance Typeable S
 
